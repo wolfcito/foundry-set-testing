9b22ca4caeefe6028a0fc2065df3cb1ad68bc3ba
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-16 00:13:35 -0500

    test to validate the transfer between to addresses

--- test/FoundryToken.t.sol
+++ test/FoundryToken.t.sol
@@ -11,17 +11,25 @@ contract FoundryTokenTest is Test {
     FoundryToken internal token;
 
     address payable internal alice;
+    address payable internal bob;
 
     function setUp() public {
         utils = new Utilities();
         token = new FoundryToken(1000);
 
         // Create a user
-        address payable[] memory users = utils.createUsers(1);
+        address payable[] memory users = utils.createUsers(2);
 
         // Assign the user 0 as Alice
         alice = users[0];
         vm.label(alice, "Alice");
+
+        // Assign the user 1 as Bob
+        bob = users[1];
+        vm.label(bob, "Bob");
+
+        // Transfer 100 tokens to Alice
+        token.transfer(alice, 100);
     }
 
     function testName() public {
@@ -45,5 +53,22 @@ contract FoundryTokenTest is Test {
         vm.startPrank(alice);
         vm.expectRevert(bytes("Ownable: caller is not the owner"));
         token.mintFor(alice, 1000);
+    }
+
+    function testSentAliceToBob() public {
+        // Make sure alice has 100 tokens
+        assertEq(token.balanceOf(alice), 100);
+
+        // Use alice as the sender
+        vm.startPrank(alice);
+
+        // Transfer the tokens
+        token.transfer(bob, 60);
+
+        // Check that the balance of alice is 40
+        assertEq(token.balanceOf(alice), 40);
+
+        // Check that the balance of bob is 60
+        assertEq(token.balanceOf(bob), 60);
     }
 }
